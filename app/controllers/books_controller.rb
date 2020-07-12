e3af4a88ea06bbfe2adfa852a80a616cdd1d7890
abe555 <riho.has3v@gmail.com>
2020-07-12 18:26:55 +0900

    Add (index & create) Action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,23 @@
 class BooksController < ApplicationController
   def top
   end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Books.new(book_params)
+  	book.save
+  	redirect_to book_path(book.id)
+  end
+
+
+
+
+  private
+  def book_params
+		params.require(:book).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+# Topページのアクション
   get '/' => 'books#top'
-  get 'books/index'
-  get 'books/new'
+# 投稿一覧と新規投稿のアクション
+  get 'books' => 'books#index'
+
+  post 'books' => 'books#create'
+
+
+
   get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
