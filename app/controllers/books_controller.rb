62d1690cafa8a694bf76a660da98890571933298
abe555 <riho.has3v@gmail.com>
2020-07-12 23:38:35 +0900

    Add success-message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,13 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	if book.save
+  		flash[:notice] = "Book was successfully updated!"
+  		redirect_to book_path(book.id)
+  	else
+  		flash[:notice] = "Erorr! Please fill in the blank!"
+  	end
+
   end
 
   def show
