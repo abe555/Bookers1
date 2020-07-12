fa2878e5388e82ad7d820b6de0f0536ddb38d8c1
abe555 <riho.has3v@gmail.com>
2020-07-12 22:13:39 +0900

    Add destroy_function

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,5 @@ Rails.application.routes.draw do
 
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
-  get 'books/show'
-  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
