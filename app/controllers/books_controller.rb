fd71ea63c94136240c35431ffa4b88e2c022fa5e
abe555 <riho.has3v@gmail.com>
2020-07-13 20:35:04 +0900

    Add Error-message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	if book.save
+  	@book = Book.new(book_params)
+  	@books = Book.all
+  	if @book.save
   		flash[:notice] = "Book was successfully updated!"
-  		redirect_to book_path(book.id)
+  		redirect_to book_path(@book.id)
   	else
-  		flash[:notice] = "Error!"
+  		render :index
   	end
 
   end
@@ -27,9 +28,15 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book.id)
+  	@book = Book.find(params[:id])
+  	@books = Book.all
+  	if @book.update(book_params)
+  		flash[:notice] = "Book was successfully updated!"
+  		redirect_to book_path(@book.id)
+  	else
+  		flash[:notice] = "Error! Fill in the blank!"
+  		render :show
+  	end
   end
 
   def destroy
